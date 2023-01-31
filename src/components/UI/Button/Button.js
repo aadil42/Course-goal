3f20a7ca067f25f81ea3603c31dd7aa17489a762
aadil42 <adilkelawala@gmail.com>
2023-02-01 04:09:50 +0530

    made the button responsive

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,9 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-font: inherit;
+
+  width: 100%;
+  font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
   color: white;
@@ -10,6 +12,9 @@ font: inherit;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  @media (min-width: 768px) {
+    width: auto; 
+  }
 
 &:focus {
   outline: none;
